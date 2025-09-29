145af00fbed209fc80a937d6cb7d1f825427793b
muhbase <67451498+takanesachio@users.noreply.github.com>
2025-09-29 18:14:04 +0700

    optimize seo

--- resources/views/mobile/template/app.blade.php
+++ resources/views/mobile/template/app.blade.php
@@ -12,7 +12,7 @@
     <meta charset="utf-8" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="google-site-verification" content="YFkitfaIixCDtJhuaU9tjLvDyaIt_yU4UA3okbWAgnI" />
+    <meta name="google-site-verification" content="YFkitfaIixCDtJhuaU9tjLvDyaIt_yU4UA3okbWAgnI" />
     <title>{{ $website->website_name }}: Situs Judi Slot Online Gacor Terpercaya 2025</title>
     <meta name="title" content="{{ $website->website_name }}: Situs Judi Slot Online Gacor Terpercaya 2025">
     <meta name="description"
